d2badabcc2f3c055d7a6f45fb8e5edaeda6e57b7
ameysunu <ameysunu00719@gmail.com>
2021-10-01 22:50:51 +0100

    navigation and list added

--- SwiftUI Basic/SwiftUI Basic/ContentView.swift
+++ SwiftUI Basic/SwiftUI Basic/ContentView.swift
@@ -9,9 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                List(devices){ result in
+                    Text("Flagship devices: \(result.device)")
+                }
+            }
+            .navigationTitle("Hello World!")
+        }
     }
+    struct iPhones: Identifiable {
+        var id = UUID()
+        var device: String
+    }
+    
+    let devices = [iPhones(device: "iPhone 13 Pro"),iPhones(device: "iPhone 12 Pro"),iPhones(device: "iPhone 11 Pro"), iPhones(device: "iPhone XS")]
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
