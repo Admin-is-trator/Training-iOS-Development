c6dc5c6f1769109b5ca8f674694c9b597ca59981
Mufti Ramdhani <muftiramdhani@gmail.com>
2021-10-01 18:57:54 +0700

    Init Project

---
+++ SwiftUI Basic/SwiftUI Basic/SwiftUI_BasicApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_BasicApp.swift
+//  SwiftUI Basic
+//
+//  Created by Mufti Ramdhani on 01/10/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_BasicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
