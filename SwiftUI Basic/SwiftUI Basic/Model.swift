4d6faaac644a72bc956a9902cc543480d4fc2ab8
Abdullah <iriandaabdullah546@gmail.com>
2021-10-02 18:29:48 +0700

    add model

---
+++ SwiftUI Basic/SwiftUI Basic/Model.swift
@@ -0,0 +1,16 @@
+//
+//  Model.swift
+//  SwiftUI Basic
+//
+//  Created by Irianda on 02/10/21.
+//
+
+import Foundation
+
+
+class akun {
+    var name : String!
+    var username : String!
+    var password : String!
+    var noTelp : Int!
+}
